457bbcf658f7d1e72e74019c8afd7c80fcced06b
beetlerom <beetlerom@gmail.com>
2012-09-30 15:41:11 +0000

    Skinned product list view on category view.
    git-svn-id: http://xe-shop.googlecode.com/svn/trunk@490 a237117d-c3a3-7e14-bcca-fd8395c59b2d

--- lang/en.lang.php
+++ lang/en.lang.php
@@ -616,4 +616,5 @@
     $lang->settings = 'Settings';
     $lang->back_to_orders = 'Back to orders';
     $lang->about_not_logged = 'To access this part of the site you have to be logged in.';
+    $lang->no_description = 'This product has no description';
 ?>
