9922fa118a0bc35567131d64074bbb12acdd69ae
dragan-dan <ddan.dragan@gmail.com>
2012-10-02 09:02:12 +0000

    Issue 121:	repair criteria count on dispShopToolManageProducts and also added some lang texts
    git-svn-id: http://xe-shop.googlecode.com/svn/trunk@510 a237117d-c3a3-7e14-bcca-fd8395c59b2d

--- lang/en.lang.php
+++ lang/en.lang.php
@@ -618,4 +618,6 @@
     $lang->about_not_logged = 'To access this part of the site you have to be logged in.';
     $lang->no_description = 'This product has no description';
     $lang->sort_by = 'Sort by';
+    $lang->products_in_shop = 'product(s) in shop';
+    $lang->products_matched_criteria = 'product(s) matched your criteria';
 ?>
