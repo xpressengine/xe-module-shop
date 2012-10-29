210ee04cf1cddbb7b519b461f678fe31eea334a7
ucorina <corina.udrescu@gmail.com>
2012-10-29 10:56:36 +0000

    Issue 172:	Make a simple UI for xe_lang to use in shop backend - added textarea support
    git-svn-id: http://xe-shop.googlecode.com/svn/trunk@659 a237117d-c3a3-7e14-bcca-fd8395c59b2d

--- libs/classes/ShopDisplay.php
+++ libs/classes/ShopDisplay.php
@@ -13,9 +13,9 @@ class ShopDisplay
     }
 
 	/**
-	 * Returns HTML code for a text input form field that supports multiple languages
+	 * Returns HTML code for a text input/textarea form field that supports multiple languages
 	 *
-	 * Returned markup contains a div with a label and an input, plus a hidden field
+	 * Returned markup contains a div with a label and an input/textarea, plus a hidden field
 	 * for the final value, plus the "translate" link
 	 *
 	 * @param      $input_name
@@ -28,19 +28,39 @@ class ShopDisplay
 	 * @param      $container_class
 	 * @return string
 	 */
-	public static function multiLanguageInput($input_name, $input_id = NULL, $input_value = NULL, $input_style, $input_class, $label_value, $label_class, $container_class)
+	private static function multiLanguageFormField($field_type
+														, $input_name, $input_id = NULL, $input_value = NULL, $input_style, $input_class
+														, $label_value, $label_class
+														, $container_class
+														, $rows = NULL, $cols = NULL
+													)
 	{
 		global $lang;
 		if($input_id) $input_id = $input_name;
 
 		$label = '<label for="' . $input_id . '" ' . ($label_class ? ' class="' . $label_class . '"' : '') . '>' . $label_value .  '</label>';
 
-		$visible_input = '<input type="text" id="' . $input_id . '"';
-		$visible_input .= ($input_class ? ' class="' . $input_class . '"' : '');
-		$visible_input .= ($input_style ? ' style="' . $input_style . '"' : '');
-		$visible_input .=  ($label_value ? ' title="' . $label_value . '"' : '');
-		$visible_input .= ($input_value ? ' value="' . $input_value . '"' : '');
-		$visible_input .= ' />';
+		if($field_type == 'input')
+		{
+			$visible_input = '<input type="text" id="' . $input_id . '"';
+			$visible_input .= ($input_class ? ' class="' . $input_class . '"' : '');
+			$visible_input .= ($input_style ? ' style="' . $input_style . '"' : '');
+			$visible_input .=  ($label_value ? ' title="' . $label_value . '"' : '');
+			$visible_input .= ($input_value ? ' value="' . $input_value . '"' : '');
+			$visible_input .= ' />';
+		}
+		else if($field_type == 'textarea')
+		{
+			$visible_input = '<textarea id="' . $input_id . '"';
+			$visible_input .= ($rows ? ' rows="' . $rows . '"' : '');
+			$visible_input .= ($cols ? ' cols="' . $cols . '"' : '');
+			$visible_input .= ($input_class ? ' class="' . $input_class . '"' : '');
+			$visible_input .= ($input_style ? ' style="' . $input_style . '"' : '');
+			$visible_input .=  ($label_value ? ' title="' . $label_value . '"' : '');
+			$visible_input .= '>';
+			$visible_input .= $input_value;
+			$visible_input .= '</textarea>';
+		}
 
 		$hidden_input = '<input type="hidden" name="' . $input_name . '"';
 		$hidden_input .= ' value="' . ShopDisplay::getMultiLanguageValue($input_value) . '"';
@@ -48,7 +68,26 @@ class ShopDisplay
 
 		$add_language_link = '<a href="#langEdit" class="translate">' . $lang->cmd_set_multilingual . '</a>';
 
-		return '<div class="multiLanguageInput ' . $container_class .'">' . $label . $visible_input . $hidden_input . $add_language_link . '</div>';
+		return '<div class="multiLanguage' . ucfirst($field_type) . ' ' . $container_class .'">' . $label . $visible_input . $hidden_input . $add_language_link . '</div>';
+	}
+
+	public static function multiLanguageInput($input_name, $input_id = NULL, $input_value = NULL, $input_style, $input_class, $label_value, $label_class, $container_class)
+	{
+		return ShopDisplay::multiLanguageFormField('input'
+			, $input_name, $input_id, $input_value, $input_style, $input_class
+			, $label_value, $label_class
+			, $container_class
+		);
+	}
+
+	public static function multiLanguageTextarea($input_name, $input_id = NULL, $input_value = NULL, $input_style, $input_class, $label_value, $label_class, $container_class, $rows, $cols)
+	{
+		return ShopDisplay::multiLanguageFormField('textarea'
+			, $input_name, $input_id, $input_value, $input_style, $input_class
+			, $label_value, $label_class
+			, $container_class
+			, $rows, $cols
+		);
 	}
 
 	public static function getMultiLanguageValue($text)
--- tpl/js/multilingual.js
+++ tpl/js/multilingual.js
@@ -37,7 +37,7 @@ function Translation(text)
     this.loadTranslationList = function(on_success) {
         var $this = this;
         var callback = function(data){
-            if(data.error || !data.lang_list) return;
+            if(data.error) return;
 
             for(var translation_index in data.lang_list)
             {
@@ -58,12 +58,24 @@ function Translation(text)
 
     // region Private methods
     var getTranslationListByName = function(name, callback) {
-        if(!name) return;
+        if(!name)
+        {
+            callback({
+                    data : { lang_list : [] }
+                });
+            return;
+        }
         jQuery.exec_json('module.getModuleAdminLangListByName', { lang_name: name, site_srl: site_srl }, callback);
     }
 
     var getTranslationListByValue = function(value, callback) {
-        if(!value) return;
+        if(!value)
+        {
+            callback({
+                data : { lang_list : [] }
+            });
+            return;
+        }
         jQuery.exec_json('module.getModuleAdminLangListByValue', { value: value, site_srl: site_srl }, callback);
     }
     // endregion
@@ -79,20 +91,45 @@ function Translation(text)
     // endregion
 }
 
-function TranslationDisplayer(multiLanguageInput)
+function TranslationDisplayer(multiLanguageFormField)
 {
     var translationsContainerID = '#multiLanguageValues';
 
     // region Public methods
     this.show = function()
     {
-        multiLanguageInput.translation.loadTranslationList(function(){
-            for(var translation_lang_code in multiLanguageInput.translation.translations_list)
+        multiLanguageFormField.translation.loadTranslationList(function(){
+            // Populate input / textareas for each language with values from the database
+            for(var translation_lang_code in multiLanguageFormField.translation.translations_list)
             {
-                jQuery(translationsContainerID).find("li." + translation_lang_code + " input").val(multiLanguageInput.translation.translations_list[translation_lang_code]);
+                jQuery(translationsContainerID)
+                    .find("li." + translation_lang_code + " " + multiLanguageFormField.field_type)
+                    .val(multiLanguageFormField.translation.translations_list[translation_lang_code])
             }
-            jQuery(translationsContainerID).find("li." + xe.current_lang + " input").val(multiLanguageInput.translation.getValue());
-            jQuery(translationsContainerID).dialog({modal: true, width: 400}).show();
+
+            // For current value, set user entered value instead of database value
+            jQuery(translationsContainerID)
+                .find("li." + xe.current_lang + " " + multiLanguageFormField.field_type)
+                .val(multiLanguageFormField.translation.getValue())
+
+            // If using input, hide textareas and show input and viceversa
+            other_field_type = (multiLanguageFormField.field_type == 'input' ? 'textarea' : 'input');
+            jQuery(translationsContainerID)
+                .find("li " + other_field_type)
+                .hide();
+            jQuery(translationsContainerID)
+                .find("li " + multiLanguageFormField.field_type)
+                .show();
+
+            // Show modal dialog
+            modal_width = multiLanguageFormField.field_type == 'input' ? 400 : 600;
+            modal_height = multiLanguageFormField.field_type == 'input' ? 300 : 400;
+            jQuery(translationsContainerID)
+                .dialog({
+                    modal: true
+                    , width: modal_width
+                    , height: modal_height})
+                .show();
         });
     }
 
@@ -104,14 +141,14 @@ function TranslationDisplayer(multiLanguageInput)
     this.saveCurrentValues = function()
     {
         var currentValues = {};
-        jQuery(translationsContainerID + " input").each(function(k,v){
+        jQuery(translationsContainerID + " " + multiLanguageFormField.field_type).each(function(k,v){
             var $this = jQuery(this);
             currentValues[$this.parent('li').attr('class')] = $this.val()
         });
 
-        multiLanguageInput.translation.translations_list = currentValues;
-        multiLanguageInput.translation.save(function() {
-            multiLanguageInput.update();
+        multiLanguageFormField.translation.translations_list = currentValues;
+        multiLanguageFormField.translation.save(function() {
+            multiLanguageFormField.update();
         });
 
     }
@@ -131,12 +168,13 @@ function TranslationDisplayer(multiLanguageInput)
  * @param inputContainer
  * @constructor
  */
-function MultiLanguageInput(inputContainer)
+function MultiLanguageFormField(inputContainer, field_type)
 {
     this.jquery_element = null;
     this.hidden_input = null;
     this.visible_input = null;
     this.translation = null;
+    this.field_type = null;
 
     this.update = function() {
         if(this.translation.isNew())
@@ -156,7 +194,19 @@ function MultiLanguageInput(inputContainer)
     __construct = function($this) {
         $this.jquery_element = inputContainer;
         $this.hidden_input = inputContainer.find("input[type='hidden']");
-        $this.visible_input = inputContainer.find("input[type='text']");
+        $this.field_type = field_type;
+
+        switch(field_type)
+        {
+            case "input":
+                $this.visible_input = inputContainer.find("input[type='text']");
+                break;
+            case "textarea":
+                $this.visible_input = inputContainer.find("textarea");
+                break;
+            default:
+                return;
+        }
 
         $this.translation = new Translation($this.hidden_input.val());
     }(this);
@@ -167,13 +217,24 @@ jQuery(document).ready(function($){
     var translationDisplayer = null;
 
     $(".multiLanguageInput input[type='text']").on("change", function(){
-        var multiLanguageInput = new MultiLanguageInput($(this).parent());
+        var multiLanguageInput = new MultiLanguageFormField($(this).parent(), 'input');
         multiLanguageInput.update();
     });
 
+    $(".multiLanguageTextarea textarea").on("change", function(){
+        var multiLanguageTextarea = new MultiLanguageFormField($(this).parent(), 'textarea');
+        multiLanguageTextarea.update();
+    });
+
     $(".multiLanguageInput a.translate").on("click", function(){
-        var multiLanguageInput = new MultiLanguageInput($(this).parent());
+        var multiLanguageInput = new MultiLanguageFormField($(this).parent(), 'input');
         translationDisplayer = new TranslationDisplayer(multiLanguageInput);
+        translationDisplayer.show();
+    });
+
+    $(".multiLanguageTextarea a.translate").on("click", function(){
+        var multiLanguageTextarea = new MultiLanguageFormField($(this).parent(), 'textarea');
+        translationDisplayer = new TranslationDisplayer(multiLanguageTextarea);
         translationDisplayer.show();
     });
 
